092fed1de4115fe7a0d844f7f142e0688b5d2eb3
Xinyun Zhao <yocchiroja20@gmail.com>
2020-09-19 14:50:58 -0400

    Updates

---
+++ precache-manifest.a842912f4bfb7da1d4d4e97e17da5706.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969d89815e403ebc8e316bb00b216a61",
+    "url": "/react-ssi-shopping-cart/index.html"
+  },
+  {
+    "revision": "36ecb005447e322f68a4",
+    "url": "/react-ssi-shopping-cart/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "c54b82eb0ebc65dafd45",
+    "url": "/react-ssi-shopping-cart/static/css/main.ac375c94.chunk.css"
+  },
+  {
+    "revision": "36ecb005447e322f68a4",
+    "url": "/react-ssi-shopping-cart/static/js/2.9ed24eb8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-ssi-shopping-cart/static/js/2.9ed24eb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c54b82eb0ebc65dafd45",
+    "url": "/react-ssi-shopping-cart/static/js/main.ded6244f.chunk.js"
+  },
+  {
+    "revision": "f961b1a247f173c6c8b5",
+    "url": "/react-ssi-shopping-cart/static/js/runtime-main.79a8360e.js"
+  }
+]);
